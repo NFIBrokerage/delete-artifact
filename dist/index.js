0a06b612b4d37599b3e97426e88da0d3eff71809
Michael Davis <mcarsondavis@gmail.com>
2020-04-15 13:40:57 -0500

    compile

--- dist/index.js
+++ dist/index.js
@@ -7946,15 +7946,19 @@ async function run() {
   try {
     const github = new GitHub(process.env.GITHUB_TOKEN);
 
+    console.log('context', context);
+
     const { owner, repo } = context.repo;
 
-    const artifactId = core.getInput('name', { required: true });
+    const runId = core.getInput('run_id', { required: true });
 
-    await github.actions.deleteArtifact({
+    const response = await github.actions.listWorkflowRunArtifacts({
       owner,
       repo,
-      artifact_id: artifactId
+      run_id: runId
     });
+
+    console.log('response', response);
   } catch (error) {
     core.setFailed(error.message);
   }
