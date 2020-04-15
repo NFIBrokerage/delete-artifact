6f714f2a60c883319852b0fe1979e33d128fa930
Michael Davis <mcarsondavis@gmail.com>
2020-04-15 13:14:27 -0500

    add some logging

--- src/delete-artifact.js
+++ src/delete-artifact.js
@@ -5,15 +5,19 @@ async function run() {
   try {
     const github = new GitHub(process.env.GITHUB_TOKEN);
 
+    console.log('context', context);
+
     const { owner, repo } = context.repo;
 
     const artifactId = core.getInput('name', { required: true });
 
-    await github.actions.deleteArtifact({
+    const response = await github.actions.deleteArtifact({
       owner,
       repo,
       artifact_id: artifactId
     });
+
+    console.log('response', response);
   } catch (error) {
     core.setFailed(error.message);
   }
